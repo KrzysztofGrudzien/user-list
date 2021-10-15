3bef67f7133456fd2a8ad36c1025a97ff9263880
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-10-15 21:56:53 +0200

    install react-bootstrap and test it

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,12 @@
+import { Button } from 'react-bootstrap';
+
 const App = () => {
-    return <h1 className='App'>App</h1>;
+    return (
+        <h1 className='App'>
+            <Button variant='primary'>Primary</Button> <Button variant='secondary'>Secondary</Button>{' '}
+            <Button variant='success'>Success</Button> <Button variant='warning'>Warning</Button>{' '}
+        </h1>
+    );
 };
 
 export default App;
