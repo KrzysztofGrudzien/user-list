545f6c6b1516fc271741a0d1b70015b217e3bf6a
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-10-15 23:04:56 +0200

    test bootstrap framework and add Montserrat font from google font

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,10 @@ import { Button } from 'react-bootstrap';
 
 const App = () => {
     return (
-        <h1 className='App'>
-            <Button variant='primary'>Primary</Button> <Button variant='secondary'>Secondary</Button>{' '}
-            <Button variant='success'>Success</Button> <Button variant='warning'>Warning</Button>{' '}
-        </h1>
+        <div className='App'>
+            <Button variant='primary'>Component from React-Bootstrap</Button>
+            <h1>Header H1 using Monstserrat font fromm Google fonts</h1>
+        </div>
     );
 };
 
