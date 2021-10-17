216fac47d8a7a0e784708f925d213045c6962415
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-10-17 14:55:03 +0200

    remove sidebar element and install axios library

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,52 @@
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
 
 const Main = () => {
+    const state = {
+        name: 'Krzysztof',
+        created: '2017-12-01 Mon 31 12:00pm',
+        species: 'Human',
+        type: 'Unknown',
+        gender: 'Male',
+        image: 'https://loremflickr.com/320/240',
+    };
+
     return (
         <main className='main'>
             <Container fluid>
                 <Container>
                     <Row>
-                        <Col lg={2}>Sidebar</Col>
-                        <Col lg={10}>Users list</Col>
+                        <Col lg={12}>
+                            <ul style={{ display: 'flex', listStyle: 'none' }}>
+                                <li>
+                                    <Card>
+                                        <Card.Img variant='top' src={state.image} />
+                                        <Card.Body>
+                                            <Card.Title>{state.name}</Card.Title>
+                                            <ul>
+                                                <li>
+                                                    <b>status:</b> {state.status}
+                                                </li>
+                                                <li>
+                                                    <b>species:</b> {state.species}
+                                                </li>
+                                                <li>
+                                                    <b>type:</b> {state.type}
+                                                </li>
+                                                <li>
+                                                    <b>gender:</b> {state.gender}
+                                                </li>
+                                            </ul>
+                                        </Card.Body>
+                                        <Card.Footer>
+                                            <small className='text-muted'>Created: {state.created}</small>
+                                        </Card.Footer>
+                                    </Card>
+                                </li>
+                            </ul>
+                        </Col>
                     </Row>
                 </Container>
             </Container>
