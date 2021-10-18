67c9dcbb7ff7da43054cd38b62dc509f73d38558
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-10-18 18:09:20 +0200

    create Pagination component, passe right props up and down, add a functionality filtering genders according to paginations

---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,27 @@
+import Pagination from 'react-bootstrap/Pagination';
+import styled from 'styled-components';
+import React, { Component } from 'react';
+
+class PaginationBasic extends Component {
+    render() {
+        let items = [];
+        for (let number = 1; number <= this.props.pages; number++) {
+            items.push(
+                <Pagination.Item key={number}>
+                    <a href='!#' onClick={() => this.props.filterPerPage(number)}>
+                        {number}
+                    </a>
+                </Pagination.Item>,
+            );
+        }
+        return <PaginationList>{items}</PaginationList>;
+    }
+}
+
+const PaginationList = styled(Pagination)`
+    justify-content: flex-end;
+    margin-bottom: -3rem;
+    margin-top: 2rem;
+`;
+
+export default PaginationBasic;
