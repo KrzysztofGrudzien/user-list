d5a58e662a777923b96c7ff5816f8ba49ff8fee4
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-10-19 04:02:21 +0200

    transform class component to the functional and use Hooks

--- src/components/FilterBar/FilterBar.js
+++ src/components/FilterBar/FilterBar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -7,53 +7,49 @@ import Col from 'react-bootstrap/Col';
 import { BsFilterRight } from 'react-icons/bs';
 import styled from 'styled-components';
 
-class FilterBar extends Component {
-    state = {
-        filter: '',
-    };
+const FilterBar = ({ filterUser }) => {
+    const [filter, setFilter] = useState('');
 
-    handleSubmitForm = e => {
+    const handleSubmitForm = e => {
         e.preventDefault();
-        this.props.filterUser(this.state.filter);
+        filterUser(filter);
     };
 
-    handleFilterGender = e => {
-        this.setState({ filter: e.target.value });
+    const handleFilterGender = e => {
+        setFilter(e.target.value);
     };
 
-    render() {
-        return (
-            <Container fluid style={styleContainer}>
-                <Container>
-                    <Form onSubmit={this.handleSubmitForm}>
-                        <Row>
-                            <Col sm='12'>
-                                <FormLabel>Filter by gender</FormLabel>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col sm={10}>
-                                <Form.Select aria-label='Floating label select' onChange={this.handleFilterGender}>
-                                    <option value='all'>All genders</option>
-                                    <option value='male'>Male</option>
-                                    <option value='female'>Female</option>
-                                    <option value='genderless'>Genderless</option>
-                                    <option value='unknown'>Unknown</option>
-                                </Form.Select>
-                            </Col>
-                            <Col sm={2}>
-                                <Button variant='primary' size='md' style={styleButton} type='submit'>
-                                    <BsFilterRight style={styleIcon} />
-                                    Filter your list
-                                </Button>
-                            </Col>
-                        </Row>
-                    </Form>
-                </Container>
+    return (
+        <Container fluid style={styleContainer}>
+            <Container>
+                <Form onSubmit={handleSubmitForm}>
+                    <Row>
+                        <Col sm='12'>
+                            <FormLabel>Filter by gender</FormLabel>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm={10}>
+                            <Form.Select aria-label='Floating label select' onChange={handleFilterGender}>
+                                <option value='all'>All genders</option>
+                                <option value='male'>Male</option>
+                                <option value='female'>Female</option>
+                                <option value='genderless'>Genderless</option>
+                                <option value='unknown'>Unknown</option>
+                            </Form.Select>
+                        </Col>
+                        <Col sm={2}>
+                            <Button variant='primary' size='md' style={styleButton} type='submit'>
+                                <BsFilterRight style={styleIcon} />
+                                Filter your list
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form>
             </Container>
-        );
-    }
-}
+        </Container>
+    );
+};
 
 const FormLabel = styled(Form.Label)`
     font-size: 0.8rem;
