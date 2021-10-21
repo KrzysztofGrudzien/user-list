dc05a551fbfbf58c315c0e87de1bb34e70212058
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-10-21 12:32:01 +0200

    add animations to the Footer component and move animations properties to the single variable

--- src/components/FilterBar/FilterBar.js
+++ src/components/FilterBar/FilterBar.js
@@ -13,6 +13,19 @@ const FilterBar = () => {
     const { filterUsers } = useContext(AppContext);
     const [filterUser, setFilterUser] = useState('all');
 
+    const animations = {
+        hidden: { y: -250, opacity: 0 },
+        show: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                duration: 0.5,
+                type: 'spring',
+                stiffness: 110,
+            },
+        },
+    };
+
     const handleSubmitForm = e => {
         e.preventDefault();
         filterUsers(filterUser);
@@ -23,11 +36,7 @@ const FilterBar = () => {
     };
 
     return (
-        <motion.div
-            initial={{ y: -250, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ delay: 1, duration: 1, type: 'spring', stiffness: 110 }}
-        >
+        <motion.div variants={animations} initial='hidden' animate='show'>
             <Container fluid style={styleContainer}>
                 <Container>
                     <Form onSubmit={handleSubmitForm}>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,22 +2,39 @@ import Container from 'react-bootstrap/Container';
 import styled from 'styled-components';
 import { AiFillGithub } from 'react-icons/ai';
 import { AiFillApi } from 'react-icons/ai';
+import { motion } from 'framer-motion';
 
 const Footer = () => {
+    const animations = {
+        hidden: { y: -250, opacity: 0 },
+        show: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                delay: 1,
+                duration: 1,
+                type: 'spring',
+                stiffness: 110,
+            },
+        },
+    };
+
     return (
-        <FooterContainer>
-            <p>
-                Made in React.js by <strong>Krzysztof Grudzień</strong> as a recruiter task
-            </p>
-            <Nav>
-                <a href='https://github.com/KrzysztofGrudzien/users-list' target='_blank' rel='noreferrer'>
-                    <AiFillGithub size='2rem' />
-                </a>
-                <a href='https://rickandmortyapi.com/documentation/' target='_blank' rel='noreferrer'>
-                    <AiFillApi size='2rem' />
-                </a>
-            </Nav>
-        </FooterContainer>
+        <motion.div variants={animations} initial='hidden' animate='show'>
+            <FooterContainer>
+                <p>
+                    Made in React.js by <strong>Krzysztof Grudzień</strong> as a recruiter task
+                </p>
+                <Nav>
+                    <a href='https://github.com/KrzysztofGrudzien/users-list' target='_blank' rel='noreferrer'>
+                        <AiFillGithub size='2rem' />
+                    </a>
+                    <a href='https://rickandmortyapi.com/documentation/' target='_blank' rel='noreferrer'>
+                        <AiFillApi size='2rem' />
+                    </a>
+                </Nav>
+            </FooterContainer>
+        </motion.div>
     );
 };
 
